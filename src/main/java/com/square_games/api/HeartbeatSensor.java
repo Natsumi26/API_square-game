370381028195771fff2176abe1e42c492ee0db4d
Natsumi26 <marion.redon26@yahoo.fr>
2026-09-10 11:41:40 +0200

    Affichage d'un nombre random sur l'endpoint GET /heartbeat

---
+++ src/main/java/com/square_games/api/HeartbeatSensor.java
@@ -0,0 +1,5 @@
+package com.square_games.api;
+
+public interface HeartbeatSensor {
+    int get();
+}
---
+++ src/main/java/com/square_games/api/RandomHeartbeat.java
@@ -0,0 +1,15 @@
+package com.square_games.api;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomHeartbeat implements HeartbeatSensor {
+
+    @Override
+    public int get() {
+        Random random = new Random();
+        return random.nextInt(40,230);
+    }
+}
